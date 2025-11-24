215ecb1a10ffdf98104e9e708c4507969b3db5a1
amitpstackforge <amitp.stackforge@gmail.com>
2025-11-24 13:35:55 +0530

    navbar fix

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule], // ✅ Fix: Added CommonModule
   templateUrl: './navbar.html'
 })
 export class NavbarComponent {
